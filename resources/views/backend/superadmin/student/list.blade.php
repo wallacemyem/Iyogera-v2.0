ea67b54996a6a0c087851c15109024ad899bfca7
Wallace Aboiyar <32508088+wallacemyem@users.noreply.github.com>
2020-05-13 17:18:44 +0100

    Update list.blade.php

--- resources/views/backend/superadmin/student/list.blade.php
+++ resources/views/backend/superadmin/student/list.blade.php
@@ -16,7 +16,7 @@
                     <tr>
                             <td>{{ $student->student->code }}</td>
                             <td>
-                            <img src="{{ asset('backend/images/student_image/'.$student->profile_pix.'.jpg') }}" alt="{{$student->name}}" height="100" width="100">
+                            <img src="{{ asset('backend/images/student_image/'.$student->profile_pix.'.jpg') }}" alt="{{$student->name}}">
             
                                 </td>
                         <td>{{ $student->student->user->name }}</td>
