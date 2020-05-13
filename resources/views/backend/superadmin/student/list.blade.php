fb6a13ca0daf65a983345e449155bad5d80faf38
Wallace Aboiyar <wallacemyem@hotmail.com>
2020-05-13 17:55:52 +0100

    Update list.blade.php

--- resources/views/backend/superadmin/student/list.blade.php
+++ resources/views/backend/superadmin/student/list.blade.php
@@ -16,7 +16,7 @@
                     <tr>
                         <td>
 
-                            <img src="{{ asset('backend/images/student_image/preview.png') }}" alt="{{$student->name}}" height="50" width="50">
+                            <img src="{{ asset('backend/images/student_image/'.$student->profile_pix.'.png') }}" alt="{{$student->name}}" height="50" width="50">
             
                         </td>
                         <td>{{ $student->student->code }}</td>
