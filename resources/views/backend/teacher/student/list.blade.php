1e9874e096ac2e4446daee9b350ff09e3839021d
Wallace Aboiyar <wallacemyem@hotmail.com>
2020-05-14 14:31:22 +0100

    student list

--- resources/views/backend/teacher/student/list.blade.php
+++ resources/views/backend/teacher/student/list.blade.php
@@ -5,27 +5,33 @@
     <table id="basic-datatable" class="table table-striped dt-responsive nowrap" width="100%">
             <thead class="thead-dark">
                     <tr>
-                        
                         <th>{{ translate('photo') }}</th>
-                        <th>{{ translate('name') }}</th>
                         <th>{{ translate('code') }}</th>
-                        
+                        <th>{{ translate('name') }}</th>
+                        <th>{{ translate('option') }}</th>
                     </tr>
             </thead>
             <tbody>
                 @foreach ($students as $student)
                     <tr>
-                            
-                            <td>
-                                @if (file_exists('https://localhost/dev/backend/images/student_image/'.$student->profile_pix.'.jpg'))
-                                    <img src="{{'https://localhost/dev/backend/images/student_image/'.$student->profile_pix.'.jpg'}}" alt="$student->name" height="50" width="50" class="rounded-circe">
-                                @else
-                                    <img src="{{ asset('backend/images/student_image/preview.png') }}" alt="" height="50">
-                                @endif
-                            </td>
-                        <td>{{ $student->student->user->name }}</td>
+                        <td>
+
+                            <img src="{{ asset('backend/images/student_image/'.$student->student->profile_pix.'.jpg') }}" alt="{{$student->name}}" height="50" width="50">
+            
+                        </td>
                         <td>{{ $student->student->code }}</td>
-                        
+                            
+                        <td>{{ $student->student->user->name }}</td>
+                        <td>
+                            <div class="btn-group mb-2">
+
+                                <button type="button" class="btn btn-icon btn-secondary btn-sm" style="margin-right:5px;" onclick="showLargeAjaxModal('{{ route('student.profile', $student->student->id) }}')" data-toggle="tooltip" data-placement="top" title="" data-original-title="{{ translate('student_profile') }}"> <i class="dripicons-checklist"></i> </button>
+
+                                <a href="{{ route('student.edit', $student->id) }}" class="btn btn-icon btn-secondary btn-sm" style="margin-right:5px;" data-toggle="tooltip" data-placement="top" title="" data-original-title="{{ translate('update_student') }}"> <i class="mdi mdi-wrench"></i> </a>
+
+                                <button type="button" class="btn btn-icon btn-dark btn-sm" style="margin-right:5px;" onclick="confirm_modal('{{ route('student.destroy', $student->id) }}', classAndSectionWiseStudents )" data-toggle="tooltip" data-placement="top" title="" data-original-title="{{ translate('delete_student') }}"> <i class="mdi mdi-window-close"></i> </button>
+                            </div>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
