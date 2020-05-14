cbdb5c7c7d7ba1e65a9116a90d6fa40189bd4c08
Wallace Aboiyar <wallacemyem@hotmail.com>
2020-05-14 12:19:08 +0100

    Update header.blade.php

--- resources/views/backend/layout/header.blade.php
+++ resources/views/backend/layout/header.blade.php
@@ -58,7 +58,7 @@
                             @if (Auth::user()->role == 'student')
                                 <img src="{{ 'backend/images/student_image/'.$student->profile_pix.'.jpg' }}" alt="{{$student->name}}" class="rounded-circle">
                             @elseif(Auth::user()->role != 'student')
-                                <img src="{{ 'backend/images/user_image/'.Auth::user()->id.'.jpg' }}" alt="{{Auth::user()->name}}" class="rounded-circle">
+                                <img src="{{ asset('backend/images/user_image/'.Auth::user()->id.'.jpg') }}" alt="{{Auth::user()->name}}" class="rounded-circle">
                             @else
                                 <img src="{{ asset('backend/images/avatar.jpg') }}" alt="user-image" class="rounded-circle">
                             @endif
