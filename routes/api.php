10643be8a570a4b923f722ff5e1a5b189dc6b704
Wallace Aboiyar <wallacemyem@hotmail.com>
2020-08-27 12:44:36 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::get('/users', function () {
 
 Route::post('login', 'Api\AuthController@login');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('api')->get('/user', function (Request $request) {
     return $request->user();
 
     	//Lessons
