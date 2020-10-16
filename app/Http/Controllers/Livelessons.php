05638a0202ffb01c2696279c07135d148fd509ac
Wallace Aboiyar <wallacemyem@hotmail.com>
2020-10-16 14:57:43 +0100

    Update Livelessons.php

--- app/Http/Controllers/Livelessons.php
+++ app/Http/Controllers/Livelessons.php
@@ -98,7 +98,7 @@ class Livelessons extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int   $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
